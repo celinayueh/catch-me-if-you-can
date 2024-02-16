4381cb9712f0b0f8270612c8127e3d974d1c002b
Celina <celinayueh12@gmail.com>
2024-02-16 17:08:27 -0500

    finished timer

--- countdown/script.js
+++ countdown/script.js
@@ -1,7 +1,7 @@
 // awesome sauce
 
 // variables
-let timeRemaining = 124; // default time in seconds
+let timePassed = -4; // default time in seconds
 let timerReady = true; // debounce for start button
 let switchSoundsEnabled = false;
 //audio
@@ -22,24 +22,14 @@ let timeoutId;
 function timerCount() {
     var dt = Date.now() - expected; // the drift (positive for overshooting)
 
-    if ((timeRemaining == 61) && switchSoundsEnabled) {
-        switchSound.play(); // Play switch side sounds
-    }
-    if (timeRemaining <= 1) { // Regular countdown
-        timerStop();
-        timerText.innerHTML = "TIME UP";
-        timerReady = true;
-    }
+    timePassed += 1;
 
-    timeRemaining -= 1;
-    if (timeRemaining <= 120) {
+    if (timePassed >= 0){
         if (!timerReady) {
-            timerText.innerHTML = timeRemaining.toString() + " seconds";
+            timerText.innerHTML = timePassed.toString() + " seconds";
         }
     }
-    if (timeRemaining == 10) {
-        lastCount.play(); // Play end countdown
-    }
+
     expected += interval;
     timeoutId = setTimeout(timerCount, Math.max(0, interval - dt));
 }
@@ -63,26 +53,10 @@ function timerStop() {
 function timerReset() {
     timerReady = true;
     clearTimeout(timeoutId);
-    timeRemaining = 124;
-    if (timeRemaining <= 120) {
-        timerText.innerHTML = timeRemaining.toString() + " seconds";
-    } else if (timeRemaining >= 120) {
-        timerText.innerHTML = "120 seconds";
-    }
+    timePassed = -4;
+    timerText.innerHTML = "0 seconds";
 }
 
-
-
-function switchCountdown() {
-    if (timerReady) {
-        switchSoundsEnabled = switchSoundsEnabled ? false : true;
-        if (switchSoundsEnabled) {
-            countdownSwitch.innerHTML = "Disable Sound at Halfway"
-        } else {
-            countdownSwitch.innerHTML = "Enable Sound at Halfway"
-        }
-    }
-}
 
 
 // button events
@@ -91,8 +65,6 @@ window.addEventListener("DOMContentLoaded", function() {
     const startBtn = document.getElementById("startBtn");
     const stopBtn = document.getElementById("stopBtn");
     const resetBtn = document.getElementById("resetBtn");
-    const countdownSwitch = document.getElementById("countdownSwitch");
-    const scoreSwitch = document.getElementById("scoreSwitch");
     const timerText = this.document.getElementById("timerText");
 
     if (startBtn) { // Check if buttons loaded on browser
@@ -100,7 +72,5 @@ window.addEventListener("DOMContentLoaded", function() {
         startBtn.addEventListener("click", timerStart)
         stopBtn.addEventListener("click", timerStop)
         resetBtn.addEventListener("click", timerReset)
-        countdownSwitch.addEventListener("click", switchCountdown)
-        scoreSwitch.addEventListener("click", showScore)
     }   
 });
